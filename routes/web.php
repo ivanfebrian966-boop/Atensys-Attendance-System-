13e4f3615db0fcab8d0207532b565e7baa4cb7cb
Muhammad Faturrahman <faturrahman290607@gmail.com>
2026-04-04 21:08:14 +0700

    migrasi tabel dan membuat view untuk halaman akun super admin

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,11 @@
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\SuperAdminController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Route::get('/home', [HomeController::class, 'home']);
-
+Route::get('/super-admin/dashboard', [SuperAdminController::class, 'index'])->name('super_admin');
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [LoginController::class, 'login'])->name('login.post');
