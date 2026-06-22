1fab569903fcc4d5317501bbdc730c08e261eb21
NUR_ILIYANIE <nuriliyanie7@gmail.com>
2026-06-22 15:13:10 +0700

    perbaiki divsi

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,15 +17,25 @@ class DatabaseSeeder extends Seeder
             'OPR' => Division::create(['division_name' => 'Operations Division']),
             'FIN' => Division::create(['division_name' => 'Finance Division']),
             'MKT' => Division::create(['division_name' => 'Marketing Division']),
+            'SLS' => Division::create(['division_name' => 'Sales Division']),
+            'LOG' => Division::create(['division_name' => 'Logistics Division']),
+            'CS'  => Division::create(['division_name' => 'Customer Service Division']),
+            'ADM' => Division::create(['division_name' => 'Administration Division']),
+            'ENG' => Division::create(['division_name' => 'Engineering Division']),
         ];
 
-        // Counter for NIP per division
+        // Counter for NIP and Email per division
         $counters = [
             'IT' => 1,
             'HRD' => 1, // HRD for HR Division
             'OPR' => 1,
             'FIN' => 1,
             'MKT' => 1,
+            'SLS' => 1,
+            'LOG' => 1,
+            'CS'  => 1,
+            'ADM' => 1,
+            'ENG' => 1,
         ];
 
         $raw_data = [
@@ -33,29 +43,29 @@ class DatabaseSeeder extends Seeder
             ['nama' => 'Akbar Zamroni', 'jk' => 'L', 'wa' => '08117092501', 'div' => 'FIN'],
             ['nama' => 'Cahyati Lamona Sitohang', 'jk' => 'P', 'wa' => '081266620445', 'div' => 'MKT'],
             ['nama' => 'Crist Garcia Pasaribu', 'jk' => 'L', 'wa' => '089623467477', 'div' => 'IT'],
-            ['nama' => 'Damar Widi Nugroho', 'jk' => 'L', 'wa' => '082169784529', 'div' => 'OPR'],
-            ['nama' => 'Dias Ferdian', 'jk' => 'L', 'wa' => '085761021014', 'div' => 'FIN'],
-            ['nama' => 'Dimas Cakra Surya Ananta', 'jk' => 'L', 'wa' => '085278458109', 'div' => 'MKT'],
-            ['nama' => 'Fathur Alfitrah Dermawan', 'jk' => 'L', 'wa' => '089635803536', 'div' => 'IT'],
-            ['nama' => 'Fazri Rahman', 'jk' => 'L', 'wa' => '085264312600', 'div' => 'OPR'],
+            ['nama' => 'Damar Widi Nugroho', 'jk' => 'L', 'wa' => '082169784529', 'div' => 'SLS'],
+            ['nama' => 'Dias Ferdian', 'jk' => 'L', 'wa' => '085761021014', 'div' => 'LOG'],
+            ['nama' => 'Dimas Cakra Surya Ananta', 'jk' => 'L', 'wa' => '085278458109', 'div' => 'CS'],
+            ['nama' => 'Fathur Alfitrah Dermawan', 'jk' => 'L', 'wa' => '089635803536', 'div' => 'ADM'],
+            ['nama' => 'Fazri Rahman', 'jk' => 'L', 'wa' => '085264312600', 'div' => 'ENG'],
             ['nama' => 'Fenni Patrik Simanjuntak', 'jk' => 'P', 'wa' => '087767351842', 'div' => 'FIN'],
             ['nama' => 'Haikal Mubaroq Zafia', 'jk' => 'L', 'wa' => '082170533270', 'div' => 'MKT'],
-            ['nama' => 'Hilda Tri Utami', 'jk' => 'P', 'wa' => '087770703580', 'div' => 'IT'],
+            ['nama' => 'Hilda Tri Utami', 'jk' => 'P', 'wa' => '087770703580', 'div' => 'SLS'],
             ['nama' => 'M Nurramadhan Irsya', 'jk' => 'L', 'wa' => '085278342106', 'div' => 'OPR'],
-            ['nama' => 'M. Luthfi Causart Azavi', 'jk' => 'L', 'wa' => '081261408239', 'div' => 'FIN'],
-            ['nama' => 'Michael Sando Turnip', 'jk' => 'L', 'wa' => '081372079904', 'div' => 'MKT'],
+            ['nama' => 'M. Luthfi Causart Azavi', 'jk' => 'L', 'wa' => '081261408239', 'div' => 'LOG'],
+            ['nama' => 'Michael Sando Turnip', 'jk' => 'L', 'wa' => '081372079904', 'div' => 'CS'],
             ['nama' => 'Muhammad Faturrahman', 'jk' => 'L', 'wa' => '085823310134', 'div' => 'IT'],
-            ['nama' => 'Muhammad Ivan Febrian', 'jk' => 'L', 'wa' => '082008982186', 'div' => 'OPR'],
-            ['nama' => 'Muradika Laksamana Putra', 'jk' => 'L', 'wa' => '089521953535', 'div' => 'FIN'],
+            ['nama' => 'Muhammad Ivan Febrian', 'jk' => 'L', 'wa' => '082008982186', 'div' => 'ADM'],
+            ['nama' => 'Muradika Laksamana Putra', 'jk' => 'L', 'wa' => '089521953535', 'div' => 'ENG'],
             ['nama' => 'Nur Iliyanie', 'jk' => 'P', 'wa' => '088271477576', 'div' => 'HRD'],
-            ['nama' => 'Rangga Surya Saputra', 'jk' => 'L', 'wa' => '081261260195', 'div' => 'MKT'],
+            ['nama' => 'Rangga Surya Saputra', 'jk' => 'L', 'wa' => '081261260195', 'div' => 'SLS'],
             ['nama' => 'Reifandra Kinadi', 'jk' => 'L', 'wa' => '081534211742', 'div' => 'IT'],
             ['nama' => 'Robi Yahya Harahap', 'jk' => 'L', 'wa' => '087754460586', 'div' => 'OPR'],
             ['nama' => 'Shofiyyah Binti Tholib Uwaini', 'jk' => 'P', 'wa' => '085761801593', 'div' => 'FIN'],
             ['nama' => 'Siti Halimah Chania', 'jk' => 'P', 'wa' => '085136863353', 'div' => 'MKT'],
-            ['nama' => 'Yohanes Armando Hubin', 'jk' => 'L', 'wa' => '083802617114', 'div' => 'IT'],
-            ['nama' => 'Zahrah Athirah Badiah', 'jk' => 'P', 'wa' => '081268721870', 'div' => 'OPR'],
-            ['nama' => 'Zaid Hasbiya Abrar', 'jk' => 'L', 'wa' => '081261442840', 'div' => 'FIN'],
+            ['nama' => 'Yohanes Armando Hubin', 'jk' => 'L', 'wa' => '083802617114', 'div' => 'ENG'],
+            ['nama' => 'Zahrah Athirah Badiah', 'jk' => 'P', 'wa' => '081268721870', 'div' => 'CS'],
+            ['nama' => 'Zaid Hasbiya Abrar', 'jk' => 'L', 'wa' => '081261442840', 'div' => 'LOG'],
         ];
 
         foreach ($raw_data as $emp) {
@@ -84,17 +94,21 @@ class DatabaseSeeder extends Seeder
                 $position = 'HR Manager';
             }
 
+            // Get current counter for this division
+            $currentCount = $counters[$prefix]++;
+            
             // Generate NIP: Prefix-00X
-            $nip = $prefix . '-' . str_pad($counters[$prefix]++, 3, '0', STR_PAD_LEFT);
+            $nip = $prefix . '-' . str_pad($currentCount, 3, '0', STR_PAD_LEFT);
 
             // Generate Email and Password
             if ($isSuperAdmin) {
                 $email = 'admin@attensys.com';
                 $password = 'admin123';
             } else {
-                $cleanName = strtolower(str_replace([' ', '`', '.', '\''], '', $emp['nama']));
-                $email = $cleanName . '@attensys.com';
-                $password = $cleanName . '123';
+                // Email format: divisionname + counter @attensys.com (e.g. operations1@attensys.com)
+                $divisionNameLower = strtolower(str_replace(' Division', '', $divisions[$divKey === 'HRD' ? 'HR' : $divKey]->division_name));
+                $email = str_replace(' ', '', $divisionNameLower) . $currentCount . '@attensys.com';
+                $password = str_replace(' ', '', $divisionNameLower) . '123';
             }
 
             Employee::create([
